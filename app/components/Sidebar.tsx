9ee598d84d13992e73eff4b319b5d3d236b37b0a
johnmillerbit <johnnydpx0@gmail.com>
2025-07-18 16:11:19 +0700

    rename lib

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSidebar } from "@/context/Sidebar";
 import Link from "next/link";
-import "../../components/icons";
+import "../../lib/icons";
 
 export default function Sidebar() {
     const { toggleSidebar, isSidebarOpen } = useSidebar();
