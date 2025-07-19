db672170ff764e63aa8396a7315a08ca191f3b26
johnmillerbit <johnnydpx0@gmail.com>
2025-07-19 20:47:17 +0700

    done

--- app/components/MarkdownRenderer.tsx
+++ app/components/MarkdownRenderer.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { marked } from "marked";
+import DOMPurify from "dompurify";
 
+// Configure marked to be synchronous
+marked.setOptions({
+    async: false,
+});
 
 interface Props {
     markdown: string;
 }
 
 const MarkdownRenderer: React.FC<Props> = ({ markdown }) => {
-    const renderedHTML = marked(markdown);
+    const renderedHTML = marked(markdown) as string; // Assert as string after setting async: false
+    const sanitizedHTML = DOMPurify.sanitize(renderedHTML);
 
     return (
         <div>
-            <div dangerouslySetInnerHTML={{ __html: renderedHTML }} />
+            <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />
         </div>
     );
 };
